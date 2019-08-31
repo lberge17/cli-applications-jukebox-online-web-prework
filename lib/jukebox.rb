554e272bdac41cb9078e49a84565d59bda8c92ea
Laura Berge <lauraeberge@gmail.com>
2019-08-31 22:07:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,25 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def say_hello(name)
+  "Hi #{name}!"
+end
+
+puts "Please enter a command:"
+command = gets.strip
+
+puts "I accept the following commands:"
+puts "- help : diplays this help message"
+puts "- list : displays a list of songs you can play" 
+puts "- play : lets you choose a song to play /n- exit : exits this program"
+puts "Please enter a command:"
+
