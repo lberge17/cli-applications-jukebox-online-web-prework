92b47e466c1beed5a347cb2d463cea20f89ed055
Laura Berge <lauraeberge@gmail.com>
2019-08-31 22:29:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,6 +21,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
+  puts user_input
   if songs.include?(user_input)
     puts "Playing #{user_input}"
   else
